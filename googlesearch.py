b8f6322e48e4cbaabe0595cb65c60a1363770f91
Aditya <aditya26sg@gmail.com>
2020-07-10 13:24:54 +0530

    second update

--- googlesearch.py
+++
@@ -1,7 +0,0 @@
-from googlesearch import search
-query='Bringham Young University Study Point'
-query = query.split()
-query = '+'.join(query)
-
-for j in search(query, tld='com', num=2, stop=1, pause=2):
-    print(j)
--- program.py
+++ program.py
@@ -2,6 +2,8 @@
 import os
 import speech_recognition as sr
 from datetime import datetime
+from selenium import webdriver
+
 
 def application(name):
     if len(name)>1:
@@ -11,7 +13,18 @@ def application(name):
     command = 'open -a "{}"'.format(str(name))
     os.system(command)
 
+def search_web(searchtype, query):
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    if searchtype != 'image':
+        driver.get(query)
+    else:
+        image_search = 'https://www.google.com/search?q='+query+'&rlz=1C5CHFA_enUS860US860&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqg__ehr3qAhUZyjgGHbcJDrgQ_AUoAXoECAwQAw&biw=1440&bih=788'
+        driver.get(image_search)
+
 keywords = ['application', 'folder', 'desktop', 'open']
+web_search = ['search', 'web']
+
 print('Be very consize and strictly say what you want to do')
 print('To open application say "open application WhatsApp"')    #This is fixed template to open an application. Do not change it
 r = sr.Recognizer()
@@ -34,11 +47,27 @@ with sr.Microphone() as source:
             f.write(audio_spoken)
             f.close()
 
-            spoken_keyword = set(audio_spoken.split()) & set(keywords)
-            if 'application' in spoken_keyword:
+            spoken_keyword1 = set(audio_spoken.split()) & set(keywords)
+            spoken_keyword2 = set(audio_spoken.split()) & set(web_search)
+
+            if 'application' in spoken_keyword1:
                 spoken_words = audio_spoken.split()
                 application(spoken_words[2:])
-            
+            if 'search' in spoken_keyword2 and 'web' in spoken_keyword2:
+                spoken_words = audio_spoken.split()
+                #search web for images of ----
+                if 'image' in spoken_words or 'images' in spoken_words:
+                    searchtype = 'image'
+                    to_search = spoken_words[4:]
+                    query = '+'.join(to_search)
+                    print(query)
+                    search_web(searchtype, query)
+                else:
+                    searchtype = 'link'
+                    to_search = spoken_words[4:]
+                    query = '+'.join(to_search)
+                    print(query)
+                    search_web(searchtype, query)
         except:
             pass
 
